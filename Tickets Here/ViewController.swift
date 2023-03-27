416acb3a50d50c00d368736d45d0d3907ecf76f4
Parth Maru <200495429@student.georgianc.on.ca>
2023-03-26 23:17:41 -0400

    Tickets Here

---
+++ Tickets Here/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Tickets Here
+//
+//  Khushboo Kodwani (301273818)
+//  Krishna Patel (301268911)
+//  Deep Mehta (301212407)
+//  Parth Maru (301209761)
+//
+//  App Description: One stop for all theater and movies information. We made an application for movie tickets booking. It will help                   user to select best theaters and movies.
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        
+        
+    }
+
+
+}
+
